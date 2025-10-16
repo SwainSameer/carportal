0a0195f4d201c673a6b033c2012d4da90419f3bf
SwainSameer <sameerswain634@gmail.com>
2025-10-16 17:11:52 +0530

    Update CarportalApplication.java
    update code

--- src/main/java/com/carportal/CarportalApplication.java
+++ src/main/java/com/carportal/CarportalApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarportalApplication {
-
+  int a =100;
+	int b=200;
 	public static void main(String[] args) {
 		SpringApplication.run(CarportalApplication.class, args);
 	}
